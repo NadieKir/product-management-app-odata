2ab9ef4995bccfe42df263896197db4b547e4537
Nadezhda Kireyenko <nadiekir@gmail.com>
2024-06-11 18:05:37 +0300

    Remove unnecessary functions

--- app/products/webapp/controller/ProductsOverviewPage.controller.js
+++ app/products/webapp/controller/ProductsOverviewPage.controller.js
@@ -4,33 +4,12 @@ sap.ui.define(
     "productmanagement/products/controller/fragments/GroupDialog",
     "productmanagement/products/controller/fragments/SortDialog",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator",
-    "sap/ui/model/Sorter",
     "sap/m/MessageBox",
     "sap/m/MessageToast",
     "productmanagement/products/model/formatter/formatter",
-    "productmanagement/products/constant/constant",
-    "productmanagement/products/helper/helper",
   ],
-  function (
-    BaseController,
-    GroupDialog,
-    SortDialog,
-    JSONModel,
-    Filter,
-    FilterOperator,
-    Sorter,
-    MessageBox,
-    MessageToast,
-    formatter,
-    constant,
-    helper
-  ) {
+  function (BaseController, GroupDialog, SortDialog, JSONModel, MessageBox, MessageToast, formatter) {
     "use strict";
-
-    const { DEFAULT_PRODUCTS_SORTER, SORT_DIALOG_NAME, GROUP_DIALOG_NAME } = constant;
-    const { areIntersecting } = helper;
 
     return BaseController.extend("productmanagement.products.controller.ProductsOverviewPage", {
       formatter,
@@ -54,44 +33,13 @@ sap.ui.define(
       },
 
       /**
-       * Controller's "before rendering" lifecycle method.
-       */
-      onBeforeRendering: function () {
-        //this.setProductsTableSorter(DEFAULT_PRODUCTS_SORTER.PATH, DEFAULT_PRODUCTS_SORTER.DESCENDING);
-      },
-
-      /**
        * Products table selection change event handler.
        */
       onTableSelectionChange: function () {
         const aSelectedItems = this.byId("idProductsTable").getSelectedItems();
-
         const aSelectedItemsData = aSelectedItems.map((oItem) => oItem.getBindingContext().getObject());
 
         this.oViewModel.setProperty("/SelectedProducts", aSelectedItemsData);
-      },
-
-      /**
-       * Product list item press event handler.
-       *
-       * @param {sap.ui.base.Event} oEvent - Event object.
-       */
-      onColumnListItemPress: function (oEvent) {
-        const oSource = oEvent.getSource();
-        const sProductPath = oSource.getBindingContext().getPath().substr(1);
-
-        this.getRouter().navTo("ProductDetailsPage", {
-          productPath: sProductPath,
-        });
-      },
-
-      /**
-       * Create product button press event handler.
-       */
-      onCreateProductButtonPress: function () {
-        this.getRouter().navTo("ProductDetailsPage", {
-          productPath: "create",
-        });
       },
 
       /**
@@ -130,194 +78,6 @@ sap.ui.define(
 
           MessageToast.show(sMessageToastText);
         }
-      },
-
-      /**
-       * Product filter bar search event handler.
-       *
-       * @param {sap.ui.base.Event} oEvent - Event object.
-       */
-      onFilterBarSearch: function (oEvent) {
-        const oProductsTableBinding = this.byId("idProductsTable").getBinding("items");
-        const aFilterFields = oEvent.getParameter("selectionSet");
-
-        const oFilter = this.createFilterFromFields(aFilterFields);
-
-        oProductsTableBinding.filter(oFilter);
-        this.updateProductsTableSelections();
-      },
-
-      /**
-       * Product filter bar reset event handler.
-       *
-       * @param {sap.ui.base.Event} oEvent - Event object.
-       */
-      onFilterBarReset: function (oEvent) {
-        const oProductsTableBinding = this.byId("idProductsTable").getBinding("items");
-        const aFilterFields = oEvent.getParameter("selectionSet");
-
-        this.resetFields(aFilterFields);
-        oProductsTableBinding.filter();
-
-        this.updateProductsTableSelections();
-      },
-
-      /**
-       * @async
-       *
-       * Products sort button press event handler.
-       */
-      onSortButtonPressed: async function () {
-        const oSortDialog = await this.getDialog(SORT_DIALOG_NAME);
-
-        oSortDialog.open();
-      },
-
-      /**
-       * @async
-       *
-       * Products group button press event handler.
-       */
-      onGroupButtonPressed: async function () {
-        const oGroupDialog = await this.getDialog(GROUP_DIALOG_NAME);
-
-        oGroupDialog.open();
-      },
-
-      /**
-       * Create filter from fields in filter bar (all filters will be applied together).
-       *
-       * @param {sap.ui.core.mvc.Controller[]} aFilterFields - Filter bar fields.
-       *
-       * @returns {sap.ui.model.Filter} - Filter from fields in filter bar.
-       */
-      createFilterFromFields: function (aFilterFields) {
-        const aFilters = [];
-
-        for (let oFilterField of aFilterFields) {
-          const sFilterName = oFilterField.getProperty("name");
-          const uFilterFieldValue = this.getFieldValue(oFilterField);
-
-          if (uFilterFieldValue === null) continue;
-
-          switch (sFilterName) {
-            case "Name":
-              aFilters.push(
-                new Filter({
-                  path: sFilterName,
-                  operator: FilterOperator.Contains,
-                  value1: uFilterFieldValue,
-                  caseSensitive: false,
-                })
-              );
-
-              break;
-
-            case "Suppliers":
-            case "Categories":
-              aFilters.push(
-                new Filter({
-                  path: sFilterName,
-                  test: (aValue) =>
-                    areIntersecting(
-                      aValue.map((oItem) => oItem.ID),
-                      uFilterFieldValue
-                    ),
-                })
-              );
-
-              break;
-
-            case "ReleaseDate":
-              aFilters.push(
-                new Filter({
-                  path: sFilterName,
-                  operator: FilterOperator.BT,
-                  value1: `\/Date(${uFilterFieldValue.startDate.getTime()})\/`,
-                  value2: `\/Date(${uFilterFieldValue.endDate.getTime()})\/`,
-                })
-              );
-
-              break;
-
-            default:
-              throw new Error(`Can't filter by product property '${sFilterName}'`);
-          }
-        }
-
-        return new Filter({
-          filters: aFilters,
-          and: true,
-        });
-      },
-
-      /**
-       * Reset filter bar fields.
-       *
-       * @param {sap.ui.core.mvc.Controller[]}} aFilterFields - Filter bar fields to reset.
-       */
-      resetFields: function (aFilterFields) {
-        aFilterFields.forEach((oFilterField) => {
-          const sFilterFieldControlName = oFilterField.getMetadata().getName();
-
-          switch (sFilterFieldControlName) {
-            case "sap.m.Input":
-            case "sap.m.DateRangeSelection":
-              oFilterField.setValue(null);
-
-              break;
-
-            case "sap.m.MultiComboBox":
-              oFilterField.removeAllSelectedItems();
-
-              break;
-
-            case "sap.m.MultiInput":
-              oFilterField.removeAllTokens();
-
-              break;
-
-            default:
-              throw new Error(`Can't reset input with name '${sFilterFieldControlName}'`);
-          }
-        });
-      },
-
-      /**
-       * Set products table sorter.
-       *
-       * @param {string} sPath - Property path to sort by.
-       * @param {boolean} bIsDescending - Whether sorting should be applied in descending order.
-       */
-      setProductsTableSorter: function (sPath, bIsDescending) {
-        const oTableBinding = this.byId("idProductsTable").getBinding("items");
-        const oGrouping = this.oViewModel.getProperty("/grouping");
-        const oSorter = new Sorter(sPath, bIsDescending);
-
-        oTableBinding.sort([oGrouping || {}, oSorter]);
-
-        this.oViewModel.setProperty("/sorter", oSorter);
-      },
-
-      /**
-       * Update products table selections.
-       */
-      updateProductsTableSelections: function () {
-        const oProductsTable = this.byId("idProductsTable");
-        const aProductsTableItems = oProductsTable.getItems();
-        const aSelectedProducts = this.oViewModel.getProperty("/SelectedProducts");
-
-        oProductsTable.removeSelections();
-
-        aSelectedProducts.forEach((oSelectedProduct) => {
-          const oProductToMarkSelected = aProductsTableItems.find((oProduct) => {
-            const oProductData = oProduct.getBindingContext().getObject();
-
-            return oProductData.ID === oSelectedProduct.ID;
-          });
-
-          oProductsTable.setSelectedItem(oProductToMarkSelected);
-        });
       },
     });
   }
--- app/products/webapp/helper/helper.js
+++ app/products/webapp/helper/helper.js
@@ -5,44 +5,6 @@ sap.ui.define(["productmanagement/products/constant/constant"], (constant) => {
 
   const helper = {
     /**
-     * Get whether arrays values are intersecting (have at least one element in common).
-     *
-     * @param {(number | string | boolean)[]} aArr - Values to search in.
-     * @param {(number | string | boolean)[]} aTargetArr - Values to be intersected.
-     *
-     * @returns {string} - Whether arrays values are intersecting.
-     */
-    areIntersecting: function (aArr, aTargetArr) {
-      return aTargetArr.some((sItem) => aArr.includes(sItem));
-    },
-
-    /**
-     * Get whether array is sorted.
-     *
-     * @param {(number | string)[]} aArr - Array to check its sorting.
-     * @param {boolean} [bDescending = false] - Whether to check sorting in descending order.
-     *
-     * @returns {boolean} - Whether array is sorted.
-     */
-    isSorted: function (aArr, bDescending = false) {
-      const bCompareAsNumbers = typeof aArr[0] === "number";
-
-      if (bCompareAsNumbers) {
-        return bDescending
-          ? aArr.every((nItem, nIndex) => nItem <= (aArr[nIndex - 1] || nItem))
-          : aArr.every((nItem, nIndex) => nItem >= (aArr[nIndex - 1] || nItem));
-      }
-
-      return bDescending
-        ? aArr.every((sItem, nIndex) => {
-            return sItem.localeCompare(aArr[nIndex + 1] || sItem) >= 0;
-          })
-        : aArr.every((sItem, nIndex) => {
-            return sItem.localeCompare(aArr[nIndex + 1] || sItem) <= 0;
-          });
-    },
-
-    /**
      * Get date object from JSON date string.
      *
      * @param {string} sJsonDate - Date string in JSON format, for example /Date(1701388800000)/.
@@ -109,53 +71,6 @@ sap.ui.define(["productmanagement/products/constant/constant"], (constant) => {
       }
 
       return null;
-    },
-
-    /**
-     * Remove chars that are not letters or numbers from string.
-     *
-     * @param {string} sString - String for keeping letters and numbers.
-     *
-     * @returns {string} - sString without chars that are not letters or numbers.
-     */
-    keepLettersAndNumbers: function (sString) {
-      if (typeof sString !== "string") return "";
-
-      return sString.replace(/[^a-zA-Z0-9]+/g, "");
-    },
-
-    /**
-     * Get timestamp string.
-     *
-     * @returns {string} - Timestamp string.
-     */
-    getTimestampString: function () {
-      const oCurrentDate = new Date();
-
-      return oCurrentDate.getTime().toString();
-    },
-
-    /**
-     * Get highest message severity.
-     *
-     * @param {sap.ui.core.message.Message[]} aMessages - Messages.
-     *
-     * @returns {sap.ui.core.message.MessageType} - Message severity.
-     */
-    getHighestMessageSeverity: function (aMessages) {
-      if (aMessages.some((oMessage) => oMessage.type === "Error")) {
-        return "Error";
-      }
-
-      if (aMessages.some((oMessage) => oMessage.type === "Warning")) {
-        return "Warning";
-      }
-
-      if (aMessages.some((oMessage) => oMessage.type === "Success")) {
-        return "Success";
-      }
-
-      return "Information";
     },
   };
 
